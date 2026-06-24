72adc606c8f10c5c78c06097600f6c196b49f184
Ross Burton <ross.burton@arm.com>
2026-06-24 14:35:59 +0200

    kas: add BB_HASHSERVE_DB_DIR to allowed environment variables
    Kas allows SSTATE_DIR to be set via an environment variable, but the
    latest oe-core will now warn if hashequiv has been enabled[1], the
    sstate is outside the build tree, but the hashequiv database is inside
    the build tree. This is because the hashequiv data is needed to make
    full use of the sstate objects.
    
    The solution here is to also set the hashserver database location, but
    this can't be done in the same way as SSTATE_DIR as we can't set it as
    an environment variable.  Add BB_HASHSERVE_DB_DIR to the list of
    variables that can be set in the environment so that callers can set
    them both if needed.
    
    [1] oe-core 491de0db64a ("sanity.bbclass: warn when sstate is outside of
        build dir, but hash equiv database is inside it")
    
    [Felix: support kas-container as well, document feature]
    
    Signed-off-by: Ross Burton <ross.burton@arm.com>
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kas/libkas.py
+++ kas/libkas.py
@@ -494,7 +494,8 @@ def get_build_environ(build_system, ctx):
 
     conf_env = ctx.config.get_environment()
 
-    env_vars = ['SSTATE_DIR', 'SSTATE_MIRRORS', 'DL_DIR', 'TMPDIR']
+    env_vars = ['SSTATE_DIR', 'SSTATE_MIRRORS', 'BB_HASHSERVE_DB_DIR',
+                'DL_DIR', 'TMPDIR']
     env_vars.extend(conf_env)
 
     env.update(conf_env)
